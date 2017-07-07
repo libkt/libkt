d4eb0e66504ea80240a68428c37c7d273b823196
Jeremy Wood <farachan@gmail.com>
2017-07-07 16:31:45 -0400

    Added CharSequence and UUID extension functions for getting players.

---
+++ src/main/kotlin/com/github/libkt/extensions/CharSequenceExtensions.kt
@@ -0,0 +1,44 @@
+package com.github.libkt.extensions
+
+import org.bukkit.Bukkit
+import org.bukkit.OfflinePlayer
+import org.bukkit.entity.Player
+import java.util.UUID
+
+/**
+ * Creates a UUID from this string using standard representation as described in the [UUID.toString] method.
+ *
+ * @return a UUID from this string representation or null if this string does not conform to the string representation
+ * as described in [UUID.toString].
+ */
+fun CharSequence.toUUID(): UUID? {
+    try {
+        return UUID.fromString(this.toString())
+    } catch(e: IllegalArgumentException) {
+        return null
+    }
+}
+
+/**
+ * Gets the online player who's name is represented by this string.
+ *
+ * It is typically better to get players by using their UUID instead since names can change between login sessions.
+ *
+ * @return the player with this name if found online, null otherwise.
+ */
+fun CharSequence.getPlayer(): Player? {
+    return Bukkit.getPlayer(this.toString())
+};
+
+/**
+ * Gets the player with this string for a name, regardless if they are offline or online.
+ *
+ * This method may involve a blocking web request to get the UUID for the this name.
+ *
+ * This will return an object even if the player does not exist. To this method, all players will exist.
+ */
+@Deprecated("Persistent storage of users should be by UUID as names are no longer unique past a single session.",
+        ReplaceWith("Bukkit.getOfflinePlayer(UUID)"), DeprecationLevel.WARNING)
+fun CharSequence.getOfflinePlayer(): OfflinePlayer {
+    return Bukkit.getOfflinePlayer(this.toString());
+}
---
+++ src/main/kotlin/com/github/libkt/extensions/UUIDExtensions.kt
@@ -0,0 +1,24 @@
+package com.github.libkt.extensions
+
+import org.bukkit.Bukkit
+import org.bukkit.OfflinePlayer
+import org.bukkit.entity.Player
+import java.util.UUID
+
+/**
+ * Gets the online player with this UUID.
+ *
+ * @return a player object if one was found with this UUID, null otherwise
+ */
+fun UUID.getPlayer(): Player? {
+    return Bukkit.getPlayer(this)
+}
+
+/**
+ * Gets the player by this UUID, regardless if they are offline or online.
+ *
+ * This will return an object even if the player does not exist. To this method, all players will exist.
+ */
+fun UUID.getOfflinePlayer(): OfflinePlayer {
+    return Bukkit.getOfflinePlayer(this);
+}
