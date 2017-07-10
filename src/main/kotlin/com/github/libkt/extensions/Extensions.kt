104a8ec7d3ee7be1bea4c6fc80a242e716b4379b
Jeremy Wood <farachan@gmail.com>
2017-07-10 14:46:32 -0400

    Added Plugin extension function to enable/disable the plugin.

--- src/main/kotlin/com/github/libkt/extensions/Extensions.kt
+++ src/main/kotlin/com/github/libkt/extensions/Extensions.kt
@@ -10,11 +10,23 @@ import org.bukkit.plugin.Plugin
 object Plugin {
 
     /**
-     * Registers all of [EventHandler]s in the given listener for this plugin.
+     * Registers all of [EventHandler]s in the given listener class for this plugin.
      *
      * @since b1
      */
     fun Plugin.registerEvents(listener: Listener) {
         server.pluginManager.registerEvents(listener, this)
+    }
+
+    /**
+     * Attempts to set whether this plugin is enabled or disabled.
+     *
+     * Any attempt to enable an already enabled plugin or disable an already disabled plugin will do nothing.
+     *
+     * @since b1
+     */
+    fun Plugin.setEnabled(enabled: Boolean) {
+        val pluginManager = server.pluginManager
+        if (enabled) pluginManager.enablePlugin(this) else pluginManager.disablePlugin(this)
     }
 }
