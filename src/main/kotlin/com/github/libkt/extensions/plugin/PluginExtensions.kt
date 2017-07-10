898fd657092d4b17801d5c904dc949be22a379fb
Jeremy Wood <farachan@gmail.com>
2017-07-10 14:50:44 -0400

    Refactored Plugin extensions again to fix imports...

---
+++ src/main/kotlin/com/github/libkt/extensions/plugin/PluginExtensions.kt
@@ -0,0 +1,29 @@
+/**
+ * Extension functions and properties that pertain to [Plugin].
+ */
+package com.github.libkt.extensions.plugin
+
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.plugin.Plugin
+
+/**
+ * Registers all of [EventHandler]s in the given listener class for this plugin.
+ *
+ * @since b1
+ */
+fun Plugin.registerEvents(listener: Listener) {
+    server.pluginManager.registerEvents(listener, this)
+}
+
+/**
+ * Attempts to set whether this plugin is enabled or disabled.
+ *
+ * Any attempt to enable an already enabled plugin or disable an already disabled plugin will do nothing.
+ *
+ * @since b1
+ */
+fun Plugin.setEnabled(enabled: Boolean) {
+    val pluginManager = server.pluginManager
+    if (enabled) pluginManager.enablePlugin(this) else pluginManager.disablePlugin(this)
+}
